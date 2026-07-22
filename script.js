9e825a64c3ddeadd1905fba514efe80b21a3fc6e
nnjnuinijnnj <abzartajhiz@gmail.com>
2026-07-23 02:47:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ document.body.style.overflow = "auto";
 },2200);
 
 };
+
+function playVideo(id){
+
+document.getElementById("player").style.display="flex";
+
+document.getElementById("video").src=
+"https://www.youtube.com/embed/"+id+"?autoplay=1";
+
+}
+
+document.getElementById("close").onclick=function(){
+
+document.getElementById("player").style.display="none";
+
+document.getElementById("video").src="";
+
+}
